8fea06f5bb31507173aa3b5e76dbed9d23a3f62e
chunshen_wu <chunshen_wu@htc.com>
2015-07-27 14:08:41 +0800

    eclipse commit

--- src/cardReader/abstractCard.java
+++ src/cardReader/abstractCard.java
@@ -3,10 +3,7 @@ package cardReader;
 public class abstractCard {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//test function
-//test remote
-		//test commit remote and local
+		// eclipse
 		
 	}
 
